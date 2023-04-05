e2d07235a3a0d62400071aa441b450460e5f4651
Jesse <jesseorhs@gmail.com>
2023-04-04 17:08:11 -0700

    Add logo assets, add content and styling to header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,20 @@
 import React from 'react';
+//images
+import Logo from '../assets/logo.svg';
+
 
 function Header () {
   return (
-    <div className='section'>Header</div>
+    <header>
+      <div className='container mx-auto'>
+        <div className='flex justify-between items-center'>
+          <a href='#'>
+            <img src={Logo} alt='logo' className='h-24 p-4' />
+          </a>
+          <button className='btn btn-sm'>Projects</button>
+        </div>
+      </div>
+    </header>
   )
 }
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,11 @@
 import React from 'react';
 //icons
-import { BiClipboard, BiHomeAlt, BiUser} from 'react-icons/bi';
+import { BiHomeAlt, BiUser} from 'react-icons/bi';
 import { BsClipboardData, BsBriefcase, BsChatSquare} from 'react-icons/bs';
 
 //link
 import { Link } from 'react-scroll';
+
 
 function Nav () {
   return (
@@ -12,20 +13,51 @@ function Nav () {
       <div className="container mx-auto">
         <div className='w-full bg-black/20 h-24 backdrop-blur-2xl mx-auto 
         rounded-full max-w-md px-5 flex items-center justify-between
-        text-2xl text-black/60'>
-          <Link to='home' className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
+        text-2xl text-white/60'>
+          <Link 
+          to='home' 
+          activeClass='active'
+          smooth={true}
+          spy={true}
+          offset={-200}
+          className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'
+          >
             <BiHomeAlt />
           </Link>
-          <Link to='about' className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
+          <Link 
+          to='about' 
+          activeClass='active'
+          smooth={true}
+          spy={true}
+          className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'
+          >
             <BiUser />
           </Link>
-          <Link to='services' className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
+          <Link 
+          to='services' 
+          activeClass='active'
+          smooth={true}
+          spy={true}
+          className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'
+          >
             <BsClipboardData />
           </Link>
-          <Link to='work' className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
+          <Link 
+          to='work' 
+          activeClass='active'
+          smooth={true}
+          spy={true}
+          className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'
+          >
             <BsBriefcase />
           </Link>
-          <Link to='contact' className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
+          <Link 
+          to='contact' 
+          activeClass='active'
+          smooth={true}
+          spy={true}
+          className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'
+          >
             <BsChatSquare />
           </Link>
         </div>
